ee2b2e7adc77dac97769aac8c57057992bfc9eb1
DQB2HHW <extern.yuri.bucci@volkswagen.com.br>
2021-04-30 13:20:46 -0300

    Markdown

--- main.tf
+++ main.tf
@@ -4,7 +4,23 @@ provider "kubernetes" {
 }
 
 
-resource "kubernetes_namespace" "minikube-namespace1" {
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.1.2"
+    }
+  }
+}
+
+
+resource "kubernetes_namespace" "minikube-namespace-db" {
   metadata {
         name = "database"
   }
@@ -28,24 +44,10 @@ resource "kubernetes_namespace" "minikube-namespace3" {
   }
 }
 
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.1.2"
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
 
 resource helm_release "mysql" {
   name       = "mysql"
-  chart      = "/mnt/c/Terraform/helm/mysql"
+  chart      = "./helm/mysql"
   namespace  = "database"
 
   set {
@@ -63,13 +65,34 @@ resource helm_release "mysql" {
       value = "/kubernetes/pv/db/mysql_cliente/"
   }
 
-  set {
-      name = "database.name"
-      value = "WPCLIENTE1"
-  }
-
-
 }
+
+resource "null_resource" "create_database_1" {
+  provisioner "local-exec" {
+    command = "kubectl -n database exec wordpress-db-mysql-0 -- mysql -u root -p secret -e CREATE DATABASE CLIENTE1; CREATE USER 'cliente1'@'%' IDENTIFIED BY 'cliente1'; GRANT ALL PRIVILEGES ON CLIENTE1.* TO 'cliente1'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  }
+}
+
+
+
+
+
+
+
+
+
+# # Configure the MySQL provider
+# provider "mysql" {
+#   endpoint = "my-database.example.com:3306"
+#   username = "app-user"
+#   password = "app-password"
+# }
+
+# # Create a Database
+# resource "mysql_database" "app" {
+#   name = "my_awesome_app"
+# }
+
 
 
 # resource helm_release "mysql2" {
