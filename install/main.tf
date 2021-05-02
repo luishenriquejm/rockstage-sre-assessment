fce68bdf1fd8d2658ec92c3bdcbe47930bb35843
Yuri Filipe Bucci <Extern.Yuri.Bucci@volkswagen.com.br>
2021-05-02 02:05:51 -0300

    Variaveis TF

--- install/main.tf
+++ install/main.tf
@@ -49,131 +49,131 @@ resource helm_release "mysql" {
 
 }
 
-#### CLIENT 1 ###
+# #### CLIENT 1 ###
 
-resource helm_release "wordpress" {
-  name       = "${var.wp1_helm_name}"
-  chart      = "./helm/wordpress"
-  namespace  = "${var.namespace_1}"
+# resource helm_release "wordpress" {
+#   name       = "${var.wp1_helm_name}"
+#   chart      = "./helm/wordpress"
+#   namespace  = "${var.namespace_1}"
 
-  set {
-      name = "phpmyadmin.nodeport"
-      value = "${var.wp1_phpmyadmin_nodeport}"
-  }
+#   set {
+#       name = "phpmyadmin.nodeport"
+#       value = "${var.wp1_phpmyadmin_nodeport}"
+#   }
 
-  set {
-      name = "appname"
-      value = "${var.wp1_app_name}"
-  }
+#   set {
+#       name = "appname"
+#       value = "${var.wp1_app_name}"
+#   }
 
-  set {
-      name = "nodePort"
-      value = "${var.wp1_nodeport}"
-  }
+#   set {
+#       name = "nodePort"
+#       value = "${var.wp1_nodeport}"
+#   }
 
-  set {
-      name = "wordpress.endereco_web"
-      value = "${var.wp_lan_ip_address}"
-  }
+#   set {
+#       name = "wordpress.endereco_web"
+#       value = "${var.wp_lan_ip_address}"
+#   }
 
-  set {
-      name = "storage.size"
-      value = "${var.wp1_storage_size}"
-  }
+#   set {
+#       name = "storage.size"
+#       value = "${var.wp1_storage_size}"
+#   }
 
-  set {
-      name = "storage.path"
-      value = "${var.wp1_storage_path}"
-  }
+#   set {
+#       name = "storage.path"
+#       value = "${var.wp1_storage_path}"
+#   }
 
-  set {
-      name = "database.name"
-      value = "${var.wp1_database_name}"
-  }
+#   set {
+#       name = "database.name"
+#       value = "${var.wp1_database_name}"
+#   }
 
-  set {
-      name = "database.root_password"
-      value = base64encode("${var.database_root_password}")
-  }
+#   set {
+#       name = "database.root_password"
+#       value = base64encode("${var.database_root_password}")
+#   }
 
-  set {
-      name = "database.host"
-      value = "${var.database_host_address}"
-  }
+#   set {
+#       name = "database.host"
+#       value = "${var.database_host_address}"
+#   }
 
-  set {
-      name = "database.usuario"
-      value = "${var.wp1_user_db}"
-  }
+#   set {
+#       name = "database.usuario"
+#       value = "${var.wp1_user_db}"
+#   }
 
-  set {
-      name = "database.usuario_senha"
-      value = "${var.wp1_user_password_db}"
-  }
-}
+#   set {
+#       name = "database.usuario_senha"
+#       value = "${var.wp1_user_password_db}"
+#   }
+# }
 
-#### CLIENT 2 ###
+# #### CLIENT 2 ###
 
-resource helm_release "wordpress_2" {
-  name       = "${var.wp2_helm_name}"
-  chart      = "./helm/wordpress"
-  namespace  = "${var.namespace_2}"
+# resource helm_release "wordpress_2" {
+#   name       = "${var.wp2_helm_name}"
+#   chart      = "./helm/wordpress"
+#   namespace  = "${var.namespace_2}"
 
-  set {
-      name = "phpmyadmin.nodeport"
-      value = "${var.wp2_phpmyadmin_nodeport}"
-  }
+#   set {
+#       name = "phpmyadmin.nodeport"
+#       value = "${var.wp2_phpmyadmin_nodeport}"
+#   }
 
-  set {
-      name = "appname"
-      value = "${var.wp2_app_name}"
-  }
+#   set {
+#       name = "appname"
+#       value = "${var.wp2_app_name}"
+#   }
 
-  set {
-      name = "nodePort"
-      value = "${var.wp2_nodeport}"
-  }
+#   set {
+#       name = "nodePort"
+#       value = "${var.wp2_nodeport}"
+#   }
 
-  set {
-      name = "wordpress.endereco_web"
-      value = "${var.wp_lan_ip_address}"
-  }
+#   set {
+#       name = "wordpress.endereco_web"
+#       value = "${var.wp_lan_ip_address}"
+#   }
 
-  set {
-      name = "storage.size"
-      value = "${var.wp2_storage_size}"
-  }
+#   set {
+#       name = "storage.size"
+#       value = "${var.wp2_storage_size}"
+#   }
 
-  set {
-      name = "storage.path"
-      value = "${var.wp2_storage_path}"
-  }
+#   set {
+#       name = "storage.path"
+#       value = "${var.wp2_storage_path}"
+#   }
 
-  set {
-      name = "database.name"
-      value = "${var.wp2_database_name}"
-  }
+#   set {
+#       name = "database.name"
+#       value = "${var.wp2_database_name}"
+#   }
 
-  set {
-      name = "database.root_password"
-      value = base64encode("${var.database_root_password}")
-  }
+#   set {
+#       name = "database.root_password"
+#       value = base64encode("${var.database_root_password}")
+#   }
 
-  set {
-      name = "database.host"
-      value = "${var.database_host_address}"
-  }
+#   set {
+#       name = "database.host"
+#       value = "${var.database_host_address}"
+#   }
 
-  set {
-      name = "database.usuario"
-      value = "${var.wp2_user_db}"
-  }
+#   set {
+#       name = "database.usuario"
+#       value = "${var.wp2_user_db}"
+#   }
 
-  set {
-      name = "database.usuario_senha"
-      value = "${var.wp2_user_password_db}"
-  }
-}
+#   set {
+#       name = "database.usuario_senha"
+#       value = "${var.wp2_user_password_db}"
+#   }
+# }
 
 #### CLIENT 3 ###
 
